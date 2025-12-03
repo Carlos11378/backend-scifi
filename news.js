2c3a555f4a5a2dd8150f83228fdabdf4f074ae8a
Carlos11378 <litteralinea@gmail.com>
2025-12-03 19:33:42 -0300

    Delete news.js

--- news.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function getNews() {
-  return [
-    { title: "Descoberta nova exolua fora do sistema solar", link: "https://example.com/noticia1" },
-    { title: "Cientistas criam protótipo de motor quântico", link: "https://example.com/noticia2" }
-  ];
-};
