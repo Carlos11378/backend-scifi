4032b625c75c44c2536767414a794b77e9070f49
Carlos11378 <litteralinea@gmail.com>
2025-12-03 14:34:39 -0300

    Create news.js

---
+++ news.js
@@ -0,0 +1,6 @@
+module.exports = async function getNews() {
+  return [
+    { title: "Descoberta nova exolua fora do sistema solar", link: "https://example.com/noticia1" },
+    { title: "Cientistas criam protótipo de motor quântico", link: "https://example.com/noticia2" }
+  ];
+};
